7ea311465f70627a59b98203ffd29eb99b40066e
vladnec2002 <vladnec2002@gmail.com>
2026-06-25 22:50:15 +0300

    Implement complete and exit button

---
+++ BonusExamples/bonus16zipfile.py
@@ -0,0 +1,12 @@
+import pathlib
+import zipfile
+
+def make_archive(filepaths, dest_dir):
+    dest_path = pathlib.Path(dest_dir, "compressed.zip")
+    with zipfile.ZipFile(dest_path, "w") as archive:
+        for filepath in filepaths:
+            filepath = pathlib.Path(filepath)
+            archive.write(filepath, arcname = filepath.name)
+
+if __name__ == '__main__':
+    make_archive(filepaths=["bonus_example_1.py", "bonus_example_2.py"], dest_dir="files")
